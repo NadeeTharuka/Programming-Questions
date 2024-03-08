fb433adad341acb4ea8d1029b917ae4409225aca
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-03-07 23:02:03 -0800

    total calculation ok

--- Gift Vouchers/GiftVoucherSolution.java
+++ Gift Vouchers/GiftVoucherSolution.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class GiftVoucherSolution {
     public static void main (String args[]) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        int total = 0;
+
+        while (true) {
+             
+            // input prices
+            String x = scanner.nextLine();
+            if (x.equalsIgnoreCase("e")) {
+                break;
+            } else {
+                // calculate total
+                if (0 < Integer.valueOf(x) && Integer.valueOf(x) < 100000 ) {
+                    total = total + Integer.valueOf(x);
+                }
+            }
+
+
+
+
+
+
+        }
+        
+
+        System.out.println(total);
+
     }
 }
