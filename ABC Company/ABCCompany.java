9245364683449f7f2bc02975b7571c8ed61d03a7
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-03-08 20:38:32 +0530

    Update ABCCompany.java

--- ABC Company/ABCCompany.java
+++ ABC Company/ABCCompany.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ABCCompany {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        int emp1units =0;
+        int emp1hours =0;
+        String line1 = scanner.nextLine();
+        emp1units = Integer.valueOf(line1.split(" ")[0]);
+        emp1hours = Integer.valueOf(line1.split(" ")[1]);
+
+        int emp1Productivity = emp1units/emp1hours;
+
+        int emp2units =0;
+        int emp2hours =0;
+        String line2 = scanner.nextLine();
+        emp2units = Integer.valueOf(line2.split(" ")[0]);
+        emp2hours = Integer.valueOf(line2.split(" ")[1]);
+
+        int emp2Productivity = emp2units/emp2hours;
+
+        System.out.println(emp1Productivity);
+        System.out.println(emp2Productivity);
     }
 }
